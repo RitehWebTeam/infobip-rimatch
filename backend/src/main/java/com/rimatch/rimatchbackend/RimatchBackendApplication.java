a551f2488f8dcd0ce38c59e685430a1f04cf012a
dominikkovacevic <dominikkovacevic6@gmail.com>
2023-11-08 19:11:30 +0100

    First backend commit

---
+++ backend/src/main/java/com/rimatch/rimatchbackend/RimatchBackendApplication.java
@@ -0,0 +1,30 @@
+package com.rimatch.rimatchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class RimatchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RimatchBackendApplication.class, args);
+	}
+
+}
+
+//Added just for first deployment
+@RestController
+class MyController {
+
+	@GetMapping("/")
+	public Map<String, String> getMessage() {
+		Map<String, String> response = new HashMap<>();
+		response.put("message", "backend running");
+		return response;
+	}
+}
---
+++ backend/src/test/java/com/rimatch/rimatchbackend/RimatchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rimatch.rimatchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RimatchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
