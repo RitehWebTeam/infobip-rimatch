c68e48b10e7a72d90538b2da4b81cec592090428
Andrej Božić <abozic389@gmail.com>
2023-11-09 16:43:36 +0100

    Modify workflow so that it syncs to AWS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>This was changed with to test AWS</h1>
       <div className="card">
         <button
           className=""
