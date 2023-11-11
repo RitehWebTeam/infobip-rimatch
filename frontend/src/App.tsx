ba94a5c540490f1a3a7d8bbca3d58efbce331428
Andrej Božić <abozic389@gmail.com>
2023-11-11 14:07:03 +0100

    Docker compose for backend local development

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,24 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+function App(): React.ReactElement {
   const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("Lodaing...");
+
+  useEffect(() => {
+    fetch('http://localhost:8080/').then(res =>res.json()).then(data => setMessage(data.message))
+  }, [])
+
 
   return (
     <>
       <div className="flex justify-between">
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -29,7 +35,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Message from backend: {message}
       </p>
     </>
   );
