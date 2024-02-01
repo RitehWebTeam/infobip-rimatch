1042e9a91f74cbf64566a42412557abc044a6c3a
Andrej Božić <abozic389@gmail.com>
2024-02-01 12:17:41 +0100

    Fix styling on match cards.

--- frontend/src/components/MatchCard.tsx
+++ frontend/src/components/MatchCard.tsx
@@ -101,13 +101,13 @@ const MatchCard = () => {
   };
   const truncatedDescription = user.description.slice(0, 100);
   return (
-    <div className="overflow-y-hidden">
+    <>
       {!isProfileOpen && (
         <MatchCardContainer>
-          <div className="relative  flex h-60 w-full justify-center rounded-xl bg-cover">
+          <div className="flex w-full justify-center rounded-xl mt-4 mb-6">
             <div
               className={cx(
-                "absolute flex h-[200px] w-[200px] items-center justify-center rounded-full",
+                "flex h-[200px] w-[200px] items-center justify-center rounded-full",
                 {
                   "bg-pink-400": user.gender === "F",
                   "bg-blue-400": user.gender === "M",
@@ -121,36 +121,38 @@ const MatchCard = () => {
               />
             </div>
           </div>
-          <div className="flex flex-col items-center w-full ">
-            <h4 className="text-5xl font-bold text-navy-700 dark:text-white">
-              {`${user.firstName}, ${user.age}`}
-            </h4>
-            <p className="font-normal text-lg dark:text-gray-200">
-              {user.location}
-            </p>
-            <div className="flex justify-center items-center mt-4 text-gray-400">
-              <p className="flex align-middle text-center text-lg overflow-hidden overflow-ellipsis">
-                {truncatedDescription}
+          <div className="flex flex-col items-center w-full justify-between flex-grow gap-6 md:max-h-[22rem]">
+            <div className="w-full flex flex-col justify-center items-center">
+              <h4 className="text-5xl font-bold text-navy-700 dark:text-white">
+                {`${user.firstName}, ${user.age}`}
+              </h4>
+              <p className="font-normal text-lg dark:text-gray-200 mt-4">
+                {user.location}
               </p>
+              <div className="flex justify-center items-center mt-4 text-gray-400">
+                <p className="flex align-middle text-center text-lg overflow-hidden overflow-ellipsis">
+                  {truncatedDescription}
+                </p>
+              </div>
+              <div>
+                <button
+                  className="mt-3 text-gray-500 hover:text-gray-300 text-lg"
+                  onClick={() => openProfile()}
+                >
+                  View profile
+                </button>
+              </div>
             </div>
-            <div>
+            <div className="flex justify-between w-full text-white pb-4 px-2">
               <button
-                className="mt-3 text-gray-500 hover:text-gray-300 text-lg"
-                onClick={() => openProfile()}
-              >
-                View profile
-              </button>
-            </div>
-            <div className="flex mt-16 flex-row justify-between w-full text-white">
-              <button
-                className="btn mt-10 fixed bottom-10 sm:bottom-36 sm:left-[46rem] sm:mt-0 hover:bg-green-600 bg-green-500 transition-color duration-300 sm:ml-4  border-green-700 rounded-full w-24 h-24 shadow-md shadow-black"
+                className=" hover:bg-green-600 bg-green-500 transition-color duration-300 sm:ml-4  border-green-700 rounded-full w-24 h-24 shadow-md shadow-black"
                 onClick={() => handleNextUser(true, user.id)}
                 disabled={lodaing}
               >
                 <CheckIcon fontSize="large" />
               </button>
               <button
-                className="btn mt-10 fixed bottom-10 sm:bottom-36 sm:right-[46rem] right-3 sm:mt-0 bg-red-500 hover:bg-red-600 transition-color duration-300 rounded-full sm:mr-2 border-red-700 btn-circle w-24 h-24 shadow-md shadow-black"
+                className="bg-red-500 hover:bg-red-600 transition-color duration-300 rounded-full sm:mr-2 border-red-700 btn-circle w-24 h-24 shadow-md shadow-black"
                 onClick={() => handleNextUser(false, user.id)}
                 disabled={lodaing}
               >
@@ -161,7 +163,7 @@ const MatchCard = () => {
         </MatchCardContainer>
       )}
       {isProfileOpen && <ProfileCard user={user} onClose={closeProfile} />}
-    </div>
+    </>
   );
 };
 
@@ -174,9 +176,9 @@ const MatchCardContainer = ({
   ...props
 }: MatchCardContainerProps) => {
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col items-center flex-grow sm:pb-12 min-h-fit max-h-[800px]  md:max-h-[700px]">
       <div
-        className="relative flex flex-col items-center rounded-[25px] border-[1px] border-black-200 h-[850px] sm:h-[700px] w-[450px] sm:w-[24rem] p-4 bg-white dark:bg-[#343030] bg-clip-border border-[#acabab33] shadow-xl shadow-black"
+        className="flex flex-col items-center rounded-[25px] border-[1px] border-black-200 flex-grow w-full sm:w-[24rem] p-4 bg-white dark:bg-[#343030] bg-clip-border border-[#acabab33] shadow-xl shadow-black"
         {...props}
       >
         {children}
--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -9,12 +9,12 @@ type ProfileCardProps = {
 
 export const ProfileCard: React.FC<ProfileCardProps> = ({ user, onClose }) => {
   return (
-    <div className="flex justify-center items-center overflow-y-hidden">
-      <div className="relative flex flex-col items-center  rounded-[25px] border-[1px] border-black-200 h-[1100px] sm:h-[700px] w-[550px] p-4 dark:bg-[#343030] bg-clip-border border-[#acabab33] shadow-xl shadow-black">
-        <div className="relative flex h-72 w-full justify-center rounded-xl bg-cover">
+    <div className="flex flex-col items-center flex-grow sm:pb-8">
+      <div className="flex flex-col items-center rounded-[25px] border-[1px] border-black-200 flex-grow sm:h-[700px] max-h-[50rem] w-full sm:w-[550px] p-4 dark:bg-[#343030] bg-clip-border border-[#acabab33] shadow-xl shadow-black">
+        <div className="flex w-full justify-center rounded-xl mt-4 mb-6">
           <div
             className={cx(
-              "absolute flex h-[250px] w-[250px] items-center justify-center rounded-full",
+              "flex h-[215px] w-[215px] items-center justify-center rounded-full",
               {
                 "bg-pink-400": user.gender === "F",
                 "bg-blue-400": user.gender === "M",
@@ -22,43 +22,45 @@ export const ProfileCard: React.FC<ProfileCardProps> = ({ user, onClose }) => {
             )}
           >
             <img
-              className="w-full object-cover rounded-full"
+              className="h-52 w-52 rounded-full"
               src={user.profileImageUrl || "/Default_pfp.svg"}
               alt=""
             />
           </div>
         </div>
-        <div className="flex flex-col  items-center w-full ">
-          <h4 className="text-4xl font-bold text-navy-700 dark:text-white lin text-center">{`${user.firstName} ${user.lastName}, ${user.age} ${user.gender}`}</h4>
-
-          <div className="mt-1 border-t border-gray-500 w-full flex justify-center">
-            <p className="text-xl dark:text-gray-200">
-              Lives in: {user.location}
+        <div className="flex flex-col items-center w-full justify-around flex-grow min-h-fit">
+          <div className="w-full flex flex-col justify-center items-center">
+            <h4 className="text-4xl font-bold text-navy-700 dark:text-white text-center">{`${user.firstName} ${user.lastName}, ${user.age} ${user.gender}`}</h4>
+            <div className="mt-1 pt-4 border-t border-gray-500 w-full flex justify-center md:w-3/4">
+              <p className="text-xl dark:text-gray-200">
+                Lives in: {user.location}
+              </p>
+            </div>
+            <p className="text-3xl font-bold mt-4">
+              About {`${user.firstName}`}
             </p>
+            <div className="flex justify-center items-center mt-1 text-gray-400">
+              <p className="flex align-middle text-center text-lg sm:text-xl text-gray-300 ">
+                {user.description}
+              </p>
+            </div>
+            <div className="flex flex-row gap-2 justify-center items-center">
+              <p className=" text-lg text-gray-300">Favorite song:</p>
+              <p className=" text-lg text-gray-300">{user.favouriteSong}</p>
+            </div>
           </div>
-          <div className="mt-1">
-            <p className="text-2xl font-bold">About {`${user.firstName}`}</p>
+          <div className="flex flex-col">
+            <div className="w-full flex flex-col items-center">
+              <p className="mt-4 border-t border-gray-500  text-center">TAGS</p>
+              <div className="mt-1">{user.tags.map((tag) => `#${tag}`)}</div>
+            </div>
+            <button
+              className="mb-7 text-gray-500 hover:text-gray-300"
+              onClick={onClose}
+            >
+              Close Profile
+            </button>
           </div>
-          <div className="flex justify-center items-center mt-1 text-gray-400">
-            <p className="flex align-middle text-center text-lg sm:text-xl text-gray-300 ">
-              {user.description}
-            </p>
-          </div>
-          <div className="flex flex-row gap-2 justify-center items-center">
-            <p className=" text-lg text-gray-300">Favorite song:</p>
-            <p className=" text-lg text-gray-300">{user.favouriteSong}</p>
-          </div>
-          <div className="fixed bottom-16 sm:bottom-44 w-full flex flex-col items-center">
-            <p className="mt-4 border-t border-gray-500  text-center">TAGS</p>
-            <div className="mt-1">{user.tags.map((tag) => `#${tag}`)}</div>
-          </div>
-
-          <button
-            className=" fixed bottom-0 sm:bottom-28 mb-7  text-gray-500 hover:text-gray-300"
-            onClick={onClose}
-          >
-            Close Profile
-          </button>
         </div>
       </div>
     </div>
