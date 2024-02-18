77be749e06f626dc121a2c0c3bc18a6468196fc9
Andrej Božić <abozic389@gmail.com>
2024-02-18 21:17:22 +0100

    Use radixui for dropdown.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,77 +1,111 @@
-import { useState } from "react";
-import { RiMatchLogo, CogIcon, HeartIcon, LogoutIcon } from "@/assets";
+import { RiMatchLogo } from "@/assets";
 import useLogout from "@/hooks/useLogout";
 import useCurrentUserContext from "@/hooks/useCurrentUser";
 import { Link } from "react-router-dom";
-import MatchIcon from "@/assets/MatchIcon";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import * as Avatar from "@radix-ui/react-avatar";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const Navbar: React.FunctionComponent = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const logout = useLogout();
   const user = useCurrentUserContext();
 
-  const handleOpenDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
+  const userInitials = user.firstName[0] + user.lastName[0];
+
   return (
-    <nav className="flex items-center relative justify-between  px-5 py-6 w-full border-b border-gray-300 dark:border-gray-700 sm:mb-8">
+    <nav className="flex items-center relative justify-between  px-5 py-6 min-w-[20rem] w-full border-b border-gray-300 dark:border-gray-700 sm:mb-8">
       <div>
         <RiMatchLogo />
       </div>
       <Link
         to="/"
-        className="text-red-500 text-4xl sm:absolute sm:w-52 sm:left-0 sm:right-0 sm:mr-auto sm:ml-auto sm:text-5xl flex justify-center font-semibold font-Pacifico"
+        className="text-red-500 text-4xl sm:absolute sm:w-52 sm:left-0 sm:right-0 sm:mr-auto sm:ml-auto sm:text-5xl flex justify-center font-semibold font-Pacifico focus-visible:outline-1 focus-visible:outline focus-visible:outline-slate-400"
       >
         RiMatch
       </Link>
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-6 items-center">
         <p className="hidden sm:block text-2xl font-Montserrat">
           {user.firstName}
         </p>
 
-        <div
-          onClick={handleOpenDropdown}
-          className="h-12 w-12 md:h-14 md:w-14 hover:ring-4 user cursor-pointer relative ring-blue-700/30 rounded-full bg-cover bg-center "
-          style={{
-            backgroundImage: `url(${
-              user.profileImageUrl || "/Default_pfp.svg"
-            })`,
-          }}
-        >
-          {isDropdownOpen && (
-            <div className="drop-down w-48 overflow-hidden text-black bg-white dark:bg-[#3b3a3a] dark:text-white rounded-md shadow absolute z-10 top-12 right-3">
-              <Link
-                to="/user/profile"
-                className="px-3 py-3 text-sm font-medium flex items-center space-x-2 hover:bg-slate-200  dark:hover:bg-slate-400"
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger
+            asChild
+            className="flex justify-center items-center focus-visible:outline-2 focus-visible:outline focus-visible:outline-slate-400 rounded-full"
+          >
+            <button type="button">
+              <Avatar.Root className="inline-flex items-center justify-center h-12 w-12 md:h-14 md:w-14 hover:ring-2 ring-red-400 select-none rounded-full">
+                <Avatar.Image
+                  src={user.profileImageUrl}
+                  alt="User profile picture"
+                  className="w-full h-full bg-cover bg-center rounded-full"
+                />
+                <Avatar.AvatarFallback
+                  className="w-full h-full flex items-center justify-center text-red-500 bg-gray-100 rounded-full border-2 border-red-500"
+                  delayMs={600}
+                >
+                  {userInitials}
+                </Avatar.AvatarFallback>
+              </Avatar.Root>
+            </button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content
+              className="w-48 text-black bg-white dark:bg-[#3b3a3a] dark:text-white rounded-md"
+              sideOffset={5}
+              align="end"
+              alignOffset={-5}
+            >
+              <DropdownMenu.Item
+                className="px-3 py-3 text-sm font-medium  hover:bg-slate-200  dark:hover:bg-slate-600 focus-visible:outline-1 focus-visible:outline focus-visible:outline-slate-400 rounded-md"
+                asChild
               >
-                <CogIcon /> <span>Profile</span>
-              </Link>
-              <Link
-                to="/matches"
-                className="px-3 py-3 text-sm font-medium flex items-center space-x-2 hover:bg-slate-400"
+                <Link to="/user/profile" className="flex items-center gap-4">
+                  <SettingsOutlinedIcon />
+                  <span>Profile</span>
+                </Link>
+              </DropdownMenu.Item>
+
+              <DropdownMenu.Item
+                className="px-3 py-3 text-sm font-medium  hover:bg-slate-200 dark:hover:bg-slate-600  focus-visible:outline-1 focus-visible:outline focus-visible:outline-slate-400 rounded-md"
+                asChild
               >
-                <MatchIcon />
-                <span>My Matches</span>
-              </Link>
-              <Link
-                to="/user/preferences"
-                className="px-3  py-3  text-sm font-medium flex items-center space-x-2 hover:bg-slate-200 dark:hover:bg-slate-400"
+                <Link to="/matches" className="flex items-center gap-4">
+                  <PeopleAltIcon />
+                  <span>My Matches</span>
+                </Link>
+              </DropdownMenu.Item>
+              <DropdownMenu.Item
+                className="px-3  py-3  text-sm font-medium hover:bg-slate-200 dark:hover:bg-slate-600 focus-visible:outline-1 focus-visible:outline focus-visible:outline-slate-400 rounded-md"
+                asChild
               >
-                <HeartIcon />
-                <span> Preferences </span>
-              </Link>
-              <div
-                onClick={() => logout()}
-                className="px-3  py-3 text-sm font-medium flex items-center space-x-2 hover:bg-slate-200  dark:hover:bg-slate-400"
+                <Link
+                  to="/user/preferences"
+                  className="flex items-center gap-4"
+                >
+                  <FavoriteBorderIcon />
+                  <span> Preferences </span>
+                </Link>
+              </DropdownMenu.Item>
+              <DropdownMenu.Item
+                className="px-3 py-3 text-sm font-medium hover:bg-slate-200  dark:hover:bg-slate-600 focus-visible:outline-1 focus-visible:outline focus-visible:outline-slate-400 rounded-md w-full"
+                asChild
               >
-                <span>
+                <button
+                  type="button"
+                  onClick={() => logout()}
+                  className="flex items-center gap-4"
+                >
                   <LogoutIcon />
-                </span>
-                <span> Logout </span>
-              </div>
-            </div>
-          )}
-        </div>
+                  <span> Logout </span>
+                </button>
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
       </div>
     </nav>
   );
