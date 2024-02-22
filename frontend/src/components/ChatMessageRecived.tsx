60f9a5e060cf73bc61ba5f1f1d6a3c678e1cdff6
Andrej Božić <abozic389@gmail.com>
2024-02-22 17:12:07 +0100

    Handling long and short messages styles.

--- frontend/src/components/ChatMessageRecived.tsx
+++ frontend/src/components/ChatMessageRecived.tsx
@@ -8,17 +8,15 @@ interface ChatMessageProps {
 
 const ChatMessage = ({ text, user }: ChatMessageProps) => {
   return (
-    <div className="items-end">
-      <div className="flex  my-2">
-        <div className="flex items-center justify-center w-10 h-10 bg-gray-300 rounded-full">
-          <UserAvatar user={user} />
-        </div>
-        <div className="flex flex-col ml-2">
-          <h3 className="text-sm font-medium ">{user.firstName}</h3>
-          <p className="text-sm bg-[#3f3d3d] dark:text-white text-white p-3 rounded-xl">
-            {text}
-          </p>
-        </div>
+    <div className="flex my-2 gap-2">
+      <div className="flex justify-center w-10 h-10 bg-gray-300 rounded-full flex-shrink-0">
+        <UserAvatar user={user} />
+      </div>
+      <div className="flex flex-col mr-12">
+        <h3 className="text-sm font-medium ">{user.firstName}</h3>
+        <p className="text-sm border border-[#E8E6EA] dark:border-[#3f3d3d] dark:bg-[#3f3d3d] dark:text-white p-2 px-4 rounded-xl">
+          {text}
+        </p>
       </div>
     </div>
   );
