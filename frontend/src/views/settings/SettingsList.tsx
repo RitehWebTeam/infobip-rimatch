7faced489802fe17f8e55c4ea25d10a860633f79
SaraKalcic <kalcics4@gmail.com>
2024-03-26 22:01:03 +0100

    Added gallery icon and title to settings card.

--- frontend/src/views/settings/SettingsList.tsx
+++ frontend/src/views/settings/SettingsList.tsx
@@ -4,6 +4,7 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { Link } from "react-router-dom";
 import * as SettingsCard from "@/components/SettingsCard";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import CollectionsIcon from "@mui/icons-material/Collections";
 
 const SettingsList = () => {
   return (
@@ -44,6 +45,16 @@ const SettingsList = () => {
           <button type="button" className="flex items-center gap-4">
             <PaletteIcon />
             <span> Theme </span>
+          </button>
+        </Link>
+
+        <Link
+          to="./gallery"
+          className="px-2 py-2 text-sm font-medium hover:bg-slate-200  dark:hover:bg-slate-600 focus-visible:outline-1 focus-visible:outline focus-visible:outline-slate-400 rounded-md w-full"
+        >
+          <button type="button" className="flex items-center gap-4">
+            <CollectionsIcon />
+            <span> Gallery </span>
           </button>
         </Link>
       </div>
