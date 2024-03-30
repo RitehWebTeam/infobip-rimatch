b95ed69f85b65572c549ffc65a8344a085d23529
Simon <simonmalnar.sm@gmail.com>
2024-03-30 11:53:53 +0100

    Added preference page and preference steps

---
+++ mobile/src/views/SetupPreferences/steps/Step1Preference.native.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, TextInput, Text } from 'react-native';
+
+const Step1Preferences = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ marginBottom: 12 }}>
+        <Text>Input your phone number</Text>
+        <TextInput
+          style={{ borderWidth: 1, borderColor: 'gray', padding: 8, borderRadius: 5 }}
+          placeholder="091 999 999"
+        />
+      </View>
+      <View style={{ marginBottom: 12 }}>
+        <Text>Where are you from?</Text>
+        <TextInput
+          style={{ borderWidth: 1, borderColor: 'gray', padding: 8, borderRadius: 5 }}
+          placeholder="Please input your location"
+        />
+      </View>
+      <View style={{ marginBottom: 12 }}>
+        <Text>Tell us about yourself:</Text>
+        <TextInput
+          multiline={true}
+          numberOfLines={4}
+          style={{ borderWidth: 1, borderColor: 'gray', padding: 8, borderRadius: 5 }}
+          placeholder="I like bees..."
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Step1Preferences;
---
+++ mobile/src/views/SetupPreferences/steps/Step3Preference.native.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+const Step3Preferences = () => {
+  const [favoriteSong, setFavoriteSong] = React.useState('');
+  const [tags, setTags] = React.useState('');
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ marginBottom: 12 }}>
+        <Text>What is your favorite song?</Text>
+        <TextInput
+          style={{ borderWidth: 1, borderColor: 'gray', padding: 8, borderRadius: 5 }}
+          placeholder="Input your favorite song"
+          value={favoriteSong}
+          onChangeText={(text) => setFavoriteSong(text)}
+        />
+      </View>
+      <View style={{ marginBottom: 12 }}>
+        <Text>Enter some tags:</Text>
+        <Text style={{ fontSize: 12, color: 'gray', marginBottom: 8 }}>(separate with spaces)</Text>
+        <TextInput
+          style={{ borderWidth: 1, borderColor: 'gray', padding: 8, borderRadius: 5 }}
+          placeholder="Enter tags"
+          value={tags}
+          onChangeText={(text) => setTags(text)}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Step3Preferences;
