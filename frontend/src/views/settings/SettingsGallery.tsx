da064bc27b088bfbc07b942d8b8ebdcee04590ba
SaraKalcic <kalcics4@gmail.com>
2024-03-30 11:04:19 +0100

    Added image gallery UI.

--- frontend/src/views/settings/SettingsGallery.tsx
+++ frontend/src/views/settings/SettingsGallery.tsx
@@ -1,52 +1,68 @@
-import { UsersService } from "@/api/users";
 import * as SettingsCard from "@/components/SettingsCard";
-//import Dropzone from "@/components/forms/Dropzone";
-import useCurrentUserContext from "@/hooks/useCurrentUser";
-import { dataURLtoFile, toBase64 } from "@/utils";
-import { /*Form,*/ Formik, FormikHelpers } from "formik";
-//import { CircularProgress } from "@mui/material";
-import * as Yup from "yup";
-
-const validationSchema = Yup.object({
-  profileImageUrl: Yup.mixed<File>()
-    .required("Required")
-    .test("fileSize", "File size must be smaller than 500KB", (value) => {
-      return value && value.size <= 500 * 1024;
-    }),
-});
+import AddCircle from "@mui/icons-material/AddCircle";
+import RemoveCircle from "@mui/icons-material/RemoveCircle";
+import Stack from "@mui/material/Stack";
+import image1 from "@/images/image1.jpg";
+import image2 from "@/images/image2.jpg";
+import image3 from "@/images/image3.jpg";
+import image4 from "@/images/image4.jpg";
+import image5 from "@/images/image5.jpg";
+import image6 from "@/images/image6.jpg";
 
 const SettingsGallery = () => {
-  const user = useCurrentUserContext();
-  const { mutateAsync: updateUser } = UsersService.useUpdateUser();
-
-  const initialValues = {
-    profileImageUrl:
-      dataURLtoFile(user.profileImageUrl, "profileImage") ?? null,
-  };
-
-  type UserProfileUpdateData = typeof initialValues;
-
-  const handleSubmit = async (
-    values: UserProfileUpdateData,
-    helpers: FormikHelpers<UserProfileUpdateData>
-  ) => {
-    await updateUser({
-      profileImageUrl: await toBase64(values.profileImageUrl!),
-    });
-    helpers.resetForm({ values });
-  };
-
   return (
     <SettingsCard.Root>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        <>
-          <SettingsCard.Header title="Gallery"></SettingsCard.Header>
-        </>
-      </Formik>
+      <>
+        <SettingsCard.Header title="Gallery">
+          <Stack direction="row" spacing={2}>
+            <button type="button">
+              <AddCircle color="inherit"></AddCircle>
+            </button>
+            <button type="button">
+              <RemoveCircle color="inherit"></RemoveCircle>
+            </button>
+          </Stack>
+        </SettingsCard.Header>
+
+        <div className="grid gap-x-4 gap-y-4 grid-cols-3">
+          <div>
+            <img
+              src={image1}
+              className="flex-initial w-46 h-48 rounded-[1rem]"
+            />
+          </div>
+          <div>
+            <img
+              src={image2}
+              className="flex-initial w-46 h-48 rounded-[1rem]"
+            />
+          </div>
+          <div>
+            <img
+              src={image3}
+              className="flex-initial w-46 h-48 rounded-[1rem]"
+            />
+          </div>
+          <div>
+            <img
+              src={image4}
+              className="flex-initial w-46 h-48 rounded-[1rem]"
+            />
+          </div>
+          <div>
+            <img
+              src={image5}
+              className="flex-initial w-46 h-48 rounded-[1rem]"
+            />
+          </div>
+          <div>
+            <img
+              src={image6}
+              className="flex-initial w-46 h-48 rounded-[1rem]"
+            />
+          </div>
+        </div>
+      </>
     </SettingsCard.Root>
   );
 };
