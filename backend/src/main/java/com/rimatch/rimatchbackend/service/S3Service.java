dee570d844421953d0537ad5d8913340c9992ddc
Andrej Božić <abozic389@gmail.com>
2024-04-12 23:51:55 +0200

    Use spring integrated package for S3.

--- backend/src/main/java/com/rimatch/rimatchbackend/service/S3Service.java
+++ backend/src/main/java/com/rimatch/rimatchbackend/service/S3Service.java
@@ -1,9 +1,12 @@
 package com.rimatch.rimatchbackend.service;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,24 +17,43 @@ import java.util.Date;
 @Service
 public class S3Service {
 
-    @Autowired
-    AmazonS3 s3Client;
-    private final String bucketName = "rimatch-images";
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
 
     public String uploadFile(MultipartFile file) throws Exception {
         String fileName = generateFileName(file);
         try {
-            s3Client.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), new ObjectMetadata())
-                    .withCannedAcl(CannedAccessControlList.PublicRead));
+
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(fileName)
+                    .build();
+
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
         } catch (IOException e) {
             throw new Exception("Could not store file " + fileName + ". Please try again!");
         }
-        return s3Client.getUrl(bucketName, fileName).toString();
+        GetUrlRequest getUrlRequest = GetUrlRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build();
+
+        return s3Client.utilities().getUrl(getUrlRequest).toString();
     }
 
     public void removeFile(String fileName) {
         try {
-            s3Client.deleteObject(bucketName, fileName);
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(fileName)
+                    .build();
+            s3Client.deleteObject(deleteObjectRequest);
         } catch (Exception e) {
             throw new RuntimeException("Could not delete file " + fileName + ". Please try again!");
         }
@@ -47,8 +69,7 @@ public class S3Service {
         return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
     }
 
-    public String getObjectFromURL(String objectURL){
-        return objectURL.substring(objectURL.lastIndexOf("/")+1);
+    public String getObjectFromURL(String objectURL) {
+        return objectURL.substring(objectURL.lastIndexOf("/") + 1);
     }
 }
-
