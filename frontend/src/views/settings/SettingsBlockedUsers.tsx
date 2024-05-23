e49809206da9cb1e432742a0c45377a4eb0c3aee
Simon <simonmalnar.sm@gmail.com>
2024-05-23 22:02:53 +0200

    Added route for Blocked Users

---
+++ frontend/src/views/settings/SettingsBlockedUsers.tsx
@@ -0,0 +1,14 @@
+import * as SettingsCard from "@/components/GenericCard";
+
+const BlockedUsers = () => {
+  return (
+    <SettingsCard.Root>
+      <SettingsCard.Header title="Blocked Users" />
+      <div className="flex flex-col w-full px-4 gap-3">
+        {/* Placeholder for future content */}
+      </div>
+    </SettingsCard.Root>
+  );
+};
+
+export default BlockedUsers;
--- frontend/src/views/settings/SettingsList.tsx
+++ frontend/src/views/settings/SettingsList.tsx
@@ -40,7 +40,7 @@ const SettingsList = () => {
           </button>
         </Link>
         <Link
-          to="./"
+          to="./blockedusers"
           className="px-2 py-2 text-sm font-medium hover:bg-slate-200  dark:hover:bg-slate-600 focus-visible:outline-1 focus-visible:outline focus-visible:outline-slate-400 rounded-md w-full"
         >
           <button type="button" className="flex items-center gap-4">
