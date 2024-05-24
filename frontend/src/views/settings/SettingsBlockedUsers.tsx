ca5eb8e31ca91089cef9f3532d9e1951c969bcbc
Simon <simonmalnar.sm@gmail.com>
2024-05-24 09:52:13 +0200

    Adding Rudimentary Blocked users card
    Card lacks functional implementation. Current card is reimplementation of matched users card.

--- frontend/src/views/settings/SettingsBlockedUsers.tsx
+++ frontend/src/views/settings/SettingsBlockedUsers.tsx
@@ -1,13 +1,74 @@
 import * as SettingsCard from "@/components/GenericCard";
 
+import { MatchesService } from "@/api/matches";
+import MatchedUser from "@/components/MatchedUser";
+import { CircularProgress } from "@mui/material";
+import { Link } from "react-router-dom";
+
 const BlockedUsers = () => {
+  const query = MatchesService.useGetMatches(); //-------------------------needs adjustment
+
+  if (query.isLoading) {
+    return (
+      <SettingsCard.Root>
+        <div className="flex justify-center items-center h-full w-full text-red-500">
+          <CircularProgress size="3rem" color="inherit" />
+        </div>
+      </SettingsCard.Root>
+    );
+  }
+
+  if (query.isError || !query.isSuccess) {
+    return <div>Error</div>;
+  }
+
+  const matches = query.data;
+
+  if (matches.length === 0) {
+    return (
+      <BlockHeader>
+        <div className="flex flex-col justify-center items-center h-full w-full text-center px-4 text-lg">
+          <span>There are no blocked users</span>
+        </div>
+      </BlockHeader>
+    );
+  }
+
   return (
-    <SettingsCard.Root>
-      <SettingsCard.Header title="Blocked Users" />
-      <div className="flex flex-col w-full px-4 gap-3">
-        {/* Placeholder for future content */}
+    <BlockHeader>
+      <div className="grid grid-cols-2 sm:grid-cols-3 w-full gap-7 mt-3 lg:max-h-[33rem] lg:overflow-y-auto">
+        {matches.map((user) => (
+          <Link key={user.id} to={`./profile/${user.id}`}>
+            <MatchedUser user={user} />
+          </Link>
+        ))}
       </div>
-    </SettingsCard.Root>
+    </BlockHeader>
+  );
+};
+
+interface BlockHeaderProps {
+  children: React.ReactNode;
+}
+
+const BlockHeader = ({ children }: BlockHeaderProps) => {
+  return (
+    <div className="flex w-full flex-grow justify-center md:pb-8">
+      <div className="bg-white dark:bg-[#343030] flex w-[40rem] flex-col h-full items-center px-10 py-7 sm:rounded-lg shadow-lg shadow-black">
+        <div className="flex w-full items-start gap-5">
+          <div className="text-black dark:text-red-500 text-4xl font-bold leading-[51px] grow shrink basis-auto">
+            Blocked users
+          </div>
+        </div>
+        <div className="text-opacity-70 text-black dark:text-white dark:text-opacity-70 w-full mt-4">
+          This is a list of people you blocked.
+        </div>
+        <div className="flex w-full items-center gap-3 mt-7 mb-3">
+          <div className="bg-gray-200 dark:bg-gray-600 w-full h-px" />
+        </div>
+        {children}
+      </div>
+    </div>
   );
 };
 
