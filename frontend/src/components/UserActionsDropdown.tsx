f30da84931fe69b2f940217e733496cfa0ff995b
SaraKalcic <kalcics4@gmail.com>
2024-06-01 16:00:05 +0200

    Added basic report user dialog

--- frontend/src/components/UserActionsDropdown.tsx
+++ frontend/src/components/UserActionsDropdown.tsx
@@ -1,11 +1,15 @@
 import MoreVert from "@mui/icons-material/MoreVert";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import DoNotDisturbIcon from "@mui/icons-material/DoNotDisturb";
+import FlagIcon from "@mui/icons-material/Flag";
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import React, { useState } from "react";
 import { ProjectedUser } from "@/types/User";
 import { useNavigate } from "react-router-dom";
 import { UsersService } from "@/api/users";
+import { Field, Form, Formik } from "formik";
+import { red } from "@mui/material/colors";
+import * as Dialog from "@radix-ui/react-dialog";
 
 interface UserActionsDropdownProps {
   user: ProjectedUser;
@@ -22,6 +26,13 @@ const UserActionsDropdown = ({ user }: UserActionsDropdownProps) => {
     navigate("..");
   };
 
+  const reportReasons = [
+    "Bullying or harassment",
+    "Pretending to be someone else",
+    "Spam",
+    "Hate speech or symbols",
+  ];
+
   return (
     <DropdownMenu.Root open={open} onOpenChange={setOpen}>
       <DropdownMenu.Trigger asChild>
@@ -32,7 +43,7 @@ const UserActionsDropdown = ({ user }: UserActionsDropdownProps) => {
         <DropdownMenu.Content
           align="end"
           sideOffset={5}
-          className="flex justify-center items-center group static outline-none bg-gray-100  dark:bg-[#585252] rounded-md min-w-16 max-w-[7rem] border border-gray-300 dark:border-[#343030] text-lg"
+          className="flex justify-center items-center grid grid-rows-2 static outline-none bg-gray-100 dark:bg-[#585252] rounded-md min-w-16 max-w-[7rem] border border-gray-300 dark:border-[#343030] text-lg"
         >
           <DialogItem
             action={blockUserAction}
@@ -55,6 +66,56 @@ const UserActionsDropdown = ({ user }: UserActionsDropdownProps) => {
               {user.firstName}?
             </AlertDialog.Description>
           </DialogItem>
+
+          <ReportItem
+            action={blockUserAction}
+            dropdownSetOpen={setOpen}
+            triggerChildren={
+              <button
+                type="button"
+                className="flex items-center gap-1 hover:bg-slate-200 dark:hover:bg-slate-600  focus-visible:outline-1 focus-visible:outline focus-visible:outline-slate-40 rounded-md px-3 py-1"
+              >
+                <FlagIcon fontSize="inherit" sx={{ color: red[700] }} />
+                <span className="text-red-700">Report</span>
+              </button>
+            }
+          >
+            <Dialog.Title className="text-lg font-semibold pb-2">
+              Select a problem to report
+            </Dialog.Title>
+            <Formik
+              initialValues={{
+                checked: "",
+              }}
+              onSubmit={(values) => alert(JSON.stringify(values, null, 2))}
+            >
+              <Form>
+                <div role="group" aria-labelledby="my-radio-group">
+                  {reportReasons.map((reason, index) => (
+                    <div className="px-4 py-2" key={index}>
+                      <label>
+                        <Field type="radio" name="checked" value={reason} />
+                        {reason}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+
+                {/* <div className="text-md font-semibold pt-6 pb-2">More options</div>
+                    <div className="flex bg-white items-center border-2 py-4 px-4 rounded-2xl">
+                      <Field
+                        className="pl-2 h-[6rem] text-black outline-none border-none bg-white w-full "
+                        type="text"
+                        id="checked"
+                        placeholder=""
+                        name="checked"
+                        as="textarea"
+                      />
+                      <button type="submit">Submit</button>
+                    </div> */}
+              </Form>
+            </Formik>
+          </ReportItem>
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
@@ -117,5 +178,55 @@ const DialogItem = React.forwardRef<HTMLDivElement, DialogItemProps>(
 );
 
 DialogItem.displayName = "DialogItem";
+
+const ReportItem = React.forwardRef<HTMLDivElement, DialogItemProps>(
+  (props, forwardedRef) => {
+    const { children, dropdownSetOpen, triggerChildren, action, ...itemProps } =
+      props;
+    return (
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <DropdownMenu.Item
+            {...itemProps}
+            ref={forwardedRef}
+            className=""
+            onSelect={(event) => {
+              event.preventDefault();
+            }}
+            asChild
+          >
+            {triggerChildren}
+          </DropdownMenu.Item>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className="fixed inset-0 bg-black/60 z-10" />
+          <Dialog.Content className="w-[90vw] max-w-[450px] max-h-[90vh] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-[#1e1e1e] rounded-md p-6 z-20">
+            {children}
+            <div className="flex justify-end w-full gap-4 mt-2">
+              <Dialog.Close asChild>
+                <button
+                  className="bg-slate-200 px-2 py-1 rounded-md text-gray-700 focus:outline-none hover:bg-gray-300"
+                  onClick={() => dropdownSetOpen(false)}
+                >
+                  Cancel
+                </button>
+              </Dialog.Close>
+              <Dialog.Close asChild>
+                <button
+                  className="bg-red-600 text-white px-3 py-1 rounded-md hover:bg-red-500"
+                  onClick={action}
+                >
+                  Confirm
+                </button>
+              </Dialog.Close>
+            </div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    );
+  }
+);
+
+ReportItem.displayName = "ReportItem";
 
 export default UserActionsDropdown;
